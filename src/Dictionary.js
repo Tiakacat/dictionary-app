c3a814b29184f61d8ddce61617acfe517b3756b5
Tiakacat <106970182+Tiakacat@users.noreply.github.com>
2022-10-25 14:27:39 +0200

    removed unnecessary css, search form added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,26 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <imput
-          type="search"
-          autoFocus={true}
-          placeholder="What is your word?"
-          // onChange={findWord}
-          className="form-control shadow w=100"
-        ></imput>
+        <div className="row">
+          <div className="col-6">
+            <input
+              type="search"
+              autoFocus={true}
+              placeholder="What is your word?"
+              // onChange={findWord}
+              className="form-control shadow w=100"
+            />
+          </div>
+          <div className="col-3">
+            <button
+              className="btn btn-outline-danger shadow"
+              type="submit"
+              value="search"
+            >
+              Let's find out!
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
