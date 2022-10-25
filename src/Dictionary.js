e36868f9fdaae724e27f4e40602dc91a65f2fc1b
Tiakacat <106970182+Tiakacat@users.noreply.github.com>
2022-10-25 21:36:40 +0200

    axios imported

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
+    let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+    axios.get(apiUrl).then(handleResponse);
+    alert(`Searching for ${keyword}`);
+  }
+
+  function typeKeyword(event) {
+    setKeyword(event.target.value);
   }
   return (
     <div className="Dictionary">
@@ -16,7 +30,7 @@ export default function Dictionary() {
               type="search"
               autoFocus={true}
               placeholder="Type your word"
-              // onChange={findWord}
+              onChange={typeKeyword}
             />
           </div>
           <div className="col-3">
