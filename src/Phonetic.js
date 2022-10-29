089479ecfb0afe2fb621b27cebd0e53037705814
Tiakacat <106970182+Tiakacat@users.noreply.github.com>
2022-10-29 19:07:28 +0200

    Phonetic.js and Photos.js components created

---
+++ src/Phonetic.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
+export default function Phonetic(props) {
+  console.log(`phonetic is ` + props.response);
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
+import Synonyms from "./Synonyms";
+import Photos from "./Photos";
 
 export default function Results(props) {
   if (props.results) {
