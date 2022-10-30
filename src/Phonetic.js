2f983d607577935f15d70b01a65bf0adba15daa4
Tiakacat <106970182+Tiakacat@users.noreply.github.com>
2022-10-30 10:54:55 +0100

    updated

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,19 @@
 import React from "react";
-// import ReactAudioPlayer from "react-audio-player";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        Listen
-      </a>
-
-      <br />
-      {props.phonetic.text}
+    <div className="row">
+      {props.response.phonetics.map(function (phonetic, index) {
+        if (phonetic.audio && phonetic.text)
+          return (
+            <div className="col-sm-6 container-fluid">
+              <ReactAudioPlayer src={phonetic.audio} controls />
+              <div key={index}>{phonetic.text}</div>
+            </div>
+          );
+        else return null;
+      })}
     </div>
   );
 }
